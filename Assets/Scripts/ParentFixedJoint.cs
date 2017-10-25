77cbf7a7340317d60d3b440ef496ecdd57fcc570
Agneya Kerure <kerure.agneya@gmail.com>
2017-10-24 20:35:00 -0400

    initial commit

---
+++ Assets/Scripts/ParentFixedJoint.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class ParentFixedJoint : MonoBehaviour {
+	SteamVR_TrackedObject trackedObject;
+	SteamVR_Controller.Device device;
+	public Transform sphere;
+
+	FixedJoint fixedJoint;
+	public Rigidbody rigidBodyAttachPoint;
+	void Awake () {
+		trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	
+	void FixedUpdate () {
+		device = SteamVR_Controller.Input((int)trackedObject.index);
+
+		if(device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad)) {
+			sphere.transform.position = new Vector3(-0.62f, 0.26f, 0.63f);
+			sphere.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+			sphere.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+		}
+	}
+
+	void OnTriggerStay(Collider col) {
+		if(fixedJoint == null && device.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
+			fixedJoint = col.gameObject.AddComponent<FixedJoint>();
+			fixedJoint.connectedBody = rigidBodyAttachPoint;
+		}
+		else if( fixedJoint != null && device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger)) {
+			GameObject go = fixedJoint.gameObject;
+			Rigidbody rigidbody = go.GetComponent<Rigidbody>();
+			Object.Destroy(fixedJoint);
+			fixedJoint = null;
+			tossObject(rigidbody);
+		}
+	}
+
+	void tossObject(Rigidbody rigidbody) {
+		Transform origin = trackedObject.origin ? trackedObject.origin : trackedObject.transform.parent;
+		if(origin != null) {
+			rigidbody.velocity = origin.TransformVector(device.velocity);
+			rigidbody.angularVelocity = origin.TransformVector(device.angularVelocity);
+		}
+        else {
+			rigidbody.velocity = device.velocity;
+			rigidbody.angularVelocity = device.angularVelocity;
+		}
+    }
+}
---
+++ Assets/Scripts/PickupParent.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class PickupParent : MonoBehaviour {
+
+	SteamVR_TrackedObject trackedObject;
+	SteamVR_Controller.Device device;
+	public Transform sphere;
+	void Awake() {
+		trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	void FixedUpdate() {
+		device = SteamVR_Controller.Input((int)trackedObject.index);
+		if(device.GetTouch (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log("You are holding down the trigger");
+		}
+
+		if(device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log("You have squeezed the trigger completely");
+		}
+
+		if(device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log("You have left the trigger");
+		}
+
+		if(device.GetPress(SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log("GetPress");
+		}
+
+		if(device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log("PressDown");
+		}
+
+		if(device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log("PressUp");
+		}
+		if(device.GetPressUp(SteamVR_Controller.ButtonMask.Touchpad)) {
+			sphere.transform.position = new Vector3(-0.62f, 0.26f, 0.63f);
+			sphere.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+			sphere.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+		}
+	}
+
+	void OnTriggerStay(Collider col) {
+		Debug.Log("yOU HAVE COLLID3D! " + col.name);
+
+		if(device.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log("You have collided with " + col.name);
+			col.attachedRigidbody.isKinematic = true;
+			col.gameObject.transform.SetParent(this.gameObject.transform);
+		}
+		if(device.GetTouchUp (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log("You have left the trigger");
+			col.gameObject.transform.SetParent(null);
+			col.attachedRigidbody.isKinematic = false;
+			
+			tossObject(col.attachedRigidbody);
+		}
+	}
+
+    void tossObject(Rigidbody rigidbody) {
+		Transform origin = trackedObject.origin ? trackedObject.origin : trackedObject.transform.parent;
+		if(origin != null) {
+			rigidbody.velocity = origin.TransformVector(device.velocity);
+			rigidbody.angularVelocity = origin.TransformVector(device.angularVelocity);
+		}
+        else {
+			rigidbody.velocity = device.velocity;
+			rigidbody.angularVelocity = device.angularVelocity;
+		}
+    }
+}
